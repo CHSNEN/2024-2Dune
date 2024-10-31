85cd79dcf91e5c672f3e08968689652ab19a16e8
CHSNEN <angela4502@naver.com>
2024-10-31 21:33:46 +0900

    1) 준비 화면 배치 display 함수 구현

---
+++ 2024-2Dune/display.c
@@ -0,0 +1,105 @@
+/*
+*  display.c:
+* 화면에 게임 정보를 출력
+* 맵, 커서, 시스템 메시지, 정보창, 자원 상태 등등
+* io.c에 있는 함수들을 사용함
+*/
+
+#include "display.h"
+#include "io.h"
+
+// 출력할 내용들의 좌상단(topleft) 좌표
+const POSITION resource_pos = { 0, 0 };
+const POSITION map_pos = { 1, 0 };
+
+
+char backbuf[MAP_HEIGHT][MAP_WIDTH] = { 0 };
+char frontbuf[MAP_HEIGHT][MAP_WIDTH] = { 0 };
+
+void project(char src[N_LAYER][MAP_HEIGHT][MAP_WIDTH], char dest[MAP_HEIGHT][MAP_WIDTH]);
+void display_resource(RESOURCE resource);
+void display_map(char map[N_LAYER][MAP_HEIGHT][MAP_WIDTH]);
+void display_cursor(CURSOR cursor);
+
+
+void display(
+	RESOURCE resource,
+	char map[N_LAYER][MAP_HEIGHT][MAP_WIDTH],
+	CURSOR cursor)
+{
+	display_resource(resource);
+	display_map(map);
+	display_cursor(cursor);
+	display_system_message(system_message);
+	display_object_info(map, cursor);
+	display_commands();
+}
+
+void display_resource(RESOURCE resource) {
+	set_color(COLOR_RESOURCE);
+	gotoxy(resource_pos);
+	printf("spice = %d/%d, population=%d/%d\n",
+		resource.spice, resource.spice_max,
+		resource.population, resource.population_max
+	);
+}
+
+// subfunction of draw_map()
+void project(char src[N_LAYER][MAP_HEIGHT][MAP_WIDTH], char dest[MAP_HEIGHT][MAP_WIDTH]) {
+	for (int i = 0; i < MAP_HEIGHT; i++) {
+		for (int j = 0; j < MAP_WIDTH; j++) {
+			for (int k = 0; k < N_LAYER; k++) {
+				if (src[k][i][j] >= 0) {
+					dest[i][j] = src[k][i][j];
+				}
+			}
+		}
+	}
+}
+
+void display_map(char map[N_LAYER][MAP_HEIGHT][MAP_WIDTH]) {
+	project(map, backbuf);
+
+	for (int i = 0; i < MAP_HEIGHT; i++) {
+		for (int j = 0; j < MAP_WIDTH; j++) {
+			if (frontbuf[i][j] != backbuf[i][j]) {
+				POSITION pos = { i, j };
+				printc(padd(map_pos, pos), backbuf[i][j], COLOR_DEFAULT);
+			}
+			frontbuf[i][j] = backbuf[i][j];
+		}
+	}
+}
+
+// frontbuf[][]에서 커서 위치의 문자를 색만 바꿔서 그대로 다시 출력
+void display_cursor(CURSOR cursor) {
+	POSITION prev = cursor.previous;
+	POSITION curr = cursor.current;
+
+	char ch = frontbuf[prev.row][prev.column];
+	printc(padd(map_pos, prev), ch, COLOR_DEFAULT);
+
+	ch = frontbuf[curr.row][curr.column];
+	printc(padd(map_pos, curr), ch, COLOR_CURSOR);
+}
+
+// 1) 준비 - 시스템 메시지 표시 함수 + 초기 메시지
+char system_message[200] = "Waiting for the command... "; // 초기 메시지를 임의로 설정
+
+void display_system_message(const char* system_message) {
+	placed(MAP_HEIGHT + 2, 0);
+	printf("System: %s\n", system_message);
+}
+
+// 1) 준비 - 상태창 함수
+void display_object_info(char src[N_LAYER][MAP_HEIGHT][MAP_WIDTH], CURSOR cursor) {
+	char selected = src[1][cursor.current.row][cursor.current.column];
+	placed(0, MAP_WIDTH + 5);
+	printf("Selected Object>> %s\n", selected);
+}
+
+// 1) 준비 - 명령창 함수
+void display_commands() {
+	placed(MAP_HEIGHT + 2, MAP_WIDTH + 5);
+	printf("Choose the commands number>> 1. Move  2. Attack  3. Harvest\n");
+}
